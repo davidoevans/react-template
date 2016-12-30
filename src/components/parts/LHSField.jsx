e22408a07b3ab15de1c2c1e706c15ad6a8cabec6
davidoevans <david.evans@modjava.com>
2016-12-30 07:12:03 -0500

    enabled non-route examples to work and fixed LHSField

--- src/components/parts/LHSField.jsx
+++ src/components/parts/LHSField.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var LHSField = React.createClass({
     getInitialState: function() {
-      return {valid: true, placeholder: "Number e.g. 1335"}
+      return {valid: true, placeholder: "Number e.g. 1335", lhs: ""}
     },
     onChange: function(e) {
       console.log("lhs: " + e.target.value);
@@ -14,14 +14,13 @@ var LHSField = React.createClass({
       } else {
         this.setState({valid: true, lhs: e.target.value});
       }
-      console.log("lhs after: " + this.state.lhs);
     },
     render: function() {
       var formClass = this.state.valid ? "form-group" : "form-group has-error"
 
       return (
         <div className={formClass}>
-          <input className="form-control" onChange={this.onClick} placeholder={this.state.placeholder} value={this.state.value}/>
+          <input className="form-control" onChange={this.onChange} placeholder={this.state.placeholder} value={this.state.value}/>
         </div>
       );
     }
