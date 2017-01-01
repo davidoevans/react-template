c190300332c90e8ee4835adcd9bc3a1277b2777f
davidoevans <david.evans@modjava.com>
2017-01-01 15:36:21 -0500

    remove unused CalculatorPage

--- src/components/pages/CalculatorPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Calculator from '../calculator/Calculator';
-
-ReactDOM.render(<Calculator />, document.getElementById('calculator'));
