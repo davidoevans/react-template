abda45fb9b0ff3ffbc1739bded3c127dcada7870
davidoevans <david.evans@modjava.com>
2017-01-01 15:31:26 -0500

    clean routes

---
+++ src/components/pages/CalculatorPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from '../calculator/Calculator';
+
+ReactDOM.render(<Calculator />, document.getElementById('calculator'));
